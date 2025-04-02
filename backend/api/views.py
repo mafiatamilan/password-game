8ce697bdfc63dc8b6e33a4fe8d0affd6fe314272
Yogs # <mafiatamilan@yahoo.com>
2025-04-02 03:37:08 -0400

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 
 from rest_framework import generics
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 from .models import User
 
 
@@ -12,3 +14,13 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UpdateScore(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'roll_no'
+    def perform_update(self, serializer):
+        user = self.get_object()
+        user.score += 100
+        user.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
