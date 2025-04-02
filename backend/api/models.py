f9e78d7e4ca4385cf5547bb35b46474d8317598f
Yogs # <mafiatamilan@yahoo.com>
2025-04-02 03:18:52 -0400

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):   
-    
-    name = models.CharField(max_length=100)
-    rollno = models.PositiveIntegerField(max_length=9)
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    roll_no = models.CharField(max_length=9,unique=True)
     department = models.CharField(max_length=50)
+    score = models.IntegerField(default=False)
